23f6f68c5c652809d11e326d10cfe1d7b0fa6398
Eunnam Ahn <baduk89@hanmail.net>
2020-05-23 19:28:50 -0400

    minor edit

--- JavaPractice/src/daysInMonth.java
+++ JavaPractice/src/daysInMonth.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
+
 public class daysInMonth {
 
 	public static void main(String[] args) {
 		
-		int year;
-		int month;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter year: ");
+		int year = sc.nextInt();
+		
+		
+		System.out.println("Enter month: ");
+		int month = sc.nextInt();
+		
+		System.out.println("Year: " + year);
+		System.out.println("Month: " + month);
 
 	}
 

7b311d40c41c6d1648f65a6c81d6d003f74aa192
Eunnam Ahn <baduk89@hanmail.net>
2020-05-25 00:43:56 -0400

    minor edit

--- JavaPractice/src/daysInMonth.java
+++ JavaPractice/src/daysInMonth.java
@@ -61,31 +61,63 @@ public class daysInMonth {
 			System.out.println(year + " is not a leap year.");
 		} 
 		
+		System.out.println("");	
 		
-		// Determine number of days for the given month using Switch statement
-		switch (month) {
-		// Cases 1, 3, 5, 7, 8, 10, 12 give 31 days
-		case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+		
+		// Determine number of days for the given month using If statement
+		// Months 1, 3, 5, 7, 8, 10, 12 give 31 days
+		if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+			
 			day = 31;
 			System.out.println("Year: " + year + "\t Month: " + month + "\t Day: " + day);
-			break;
 		
-		// Case 2 gives either 29 or 28 days
-		case 2: 
-			if (leap == true) {
-				day = 29;
-				System.out.println("Year: " + year + "\t Month: " + month + "\t Day: " + day);
-			} else {
-				day = 28;
-				System.out.println("Year: " + year + "\t Month: " + month + "\t Day: " + day);
-			}
-			break;
-		
-		// Cases 4, 6, 9, and 11 give 30 days
-		case 4: case 6: case 9: case 11:
+		// Months 4, 6, 9, and 11 give 30 days
+		} else if (month == 4 || month == 6 || month == 9 || month == 11) {
+			
 			day = 30;
 			System.out.println("Year: " + year + "\t Month: " + month + "\t Day: " + day);
-			break;
+		
+		// Month 2 gives either 28 or 29 days	
+		} else if (month == 2 && leap == true) {
+			
+			day = 29;
+			System.out.println("Year: " + year + "\t Month: " + month + "\t Day: " + day);
+			
+		} else {
+			
+			day = 28;
+			System.out.println("Year: " + year + "\t Month: " + month + "\t Day: " + day);
+		
 		}
+		
+		
+//		// Determine number of days for the given month using Switch statement
+//		switch (month) {
+//		// Cases 1, 3, 5, 7, 8, 10, and 12 give 31 days
+//		case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+//			day = 31;
+//			System.out.println("Year: " + year + "\t Month: " + month + "\t Day: " + day);
+//			break;
+//		
+//		// Case 2 gives either 28 or 29 days
+//		case 2: 
+//			if (leap == true) {
+//				day = 29;
+//				System.out.println("Year: " + year + "\t Month: " + month + "\t Day: " + day);
+//			} else {
+//				day = 28;
+//				System.out.println("Year: " + year + "\t Month: " + month + "\t Day: " + day);
+//			}
+//			break;
+//		
+//		// Cases 4, 6, 9, and 11 give 30 days
+//		case 4: case 6: case 9: case 11:
+//			day = 30;
+//			System.out.println("Year: " + year + "\t Month: " + month + "\t Day: " + day);
+//			break;
+//		}
+		
+		
+		
 	}
 }

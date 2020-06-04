c5504122e047ba42ff45c849dde9ff6828bbd9f8
Eunnam Ahn <baduk89@hanmail.net>
2020-06-04 01:34:18 -0400

    Minor Edit

--- JavaPractice/src/iceCreamSelection.java
+++ JavaPractice/src/iceCreamSelection.java
@@ -11,18 +11,49 @@ public class iceCreamSelection {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
+		System.out.println("Welcome to Berkey Creamery!");
 		System.out.println("Which flavor would you like to have?");
 		String flavors = scan.next();
+		scan.close();
 		
 		String alumni = IceCream.ALUMNISWIRL.toString();
+		String death = IceCream.DEATHBYCHOCOLATE.toString();
+		String grilled = IceCream.GRILLEDSTICKIES.toString();
+		String mint = IceCream.MINTNITTANY.toString();
+		String peachy = IceCream.PEACHYPATERNO.toString();
+		String scholars = IceCream.SCHOLARSCHIP.toString();
+		String white = IceCream.WHITEOUT.toString();
+		
 		
 		if (flavors.equals(alumni)) {
 			
-			System.out.println("You have selected 'Alumni Swirl'");
+			System.out.println("You have selected 'Alumni Swirl'.");
 			
+		} else if (flavors.equals(death))  {
+			
+			System.out.println("You have selected 'Death By Chocolate'.");
+			
+		} else if (flavors.equals(grilled))  {
+			
+			System.out.println("You have selected 'Grilled Stickies'.");
+		
+		} else if (flavors.equals(mint))  {
+			
+			System.out.println("You have selected 'Mint Nittany'.");
+		
+		} else if (flavors.equals(peachy))  {
+			
+			System.out.println("You have selected 'Peachy Paterno'.");
+		
+		} else if (flavors.equals(scholars))  {
+			
+			System.out.println("You have selected 'Scholar's Chip'.");
+		
 		} else {
-			System.out.println(alumni);
-		}
+			
+			System.out.println("You have selected 'White Out'.");
+		
+		} 
 		
 	}
 

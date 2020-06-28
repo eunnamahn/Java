64f1c3c92a3e67735a15663ad1f9bde9e095c481
Eunnam Ahn <baduk89@hanmail.net>
2020-06-28 09:23:19 -0400

    Minor edit

--- JavaPractice/src/isPrime.java
+++ JavaPractice/src/isPrime.java
@@ -2,9 +2,29 @@ public class isPrime {
 
 	public static void main(String[] args) {
 		
-		boolean isPrime = true;
-		int number;
-
+		System.out.println("First few prime numbers: ");
+		
+		for (int number = 2; number <= 100; number++)
+		{
+			boolean isPrime = true;
+			
+			for (int i = 2; i <= number/2; i++)
+			{
+				if (number % i == 0)
+				{
+					isPrime = false;
+					break;
+				} 
+			}
+			
+			if (isPrime == true)
+			{
+				System.out.println(number);
+			}
+				
+			
+		}
+				
 	}
 
 }

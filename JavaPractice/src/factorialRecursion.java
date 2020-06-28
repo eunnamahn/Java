9c57867c88a9d4551ab05363dcdf15a93501159e
Eunnam Ahn <baduk89@hanmail.net>
2020-06-28 10:23:54 -0400

    First commit

---
+++ JavaPractice/src/factorialRecursion.java
@@ -0,0 +1,24 @@
+
+public class factorialRecursion {
+
+	public static void main(String[] args) {
+		
+		int n = 6;
+		System.out.println("For n = " + n + ": ");
+		System.out.println(fib(n));
+
+	}
+	
+	static int fib(int n) {
+		
+		if (n == 1) {
+			return 0;
+		} else if (n == 2) {
+			return 1;
+		} else {
+			return fib(n-1) + fib(n-2);
+		}
+				 
+	}
+
+}

8493ef9183556dd1b936230ec997fa14179ca4b2
Eunnam Ahn <baduk89@hanmail.net>
2020-06-30 18:13:25 -0400

    First commit

---
+++ JavaPractice/src/bubbleSort.java
@@ -0,0 +1,33 @@
+public class bubbleSort {
+
+	public static void main(String[] args) {
+		
+		int[] array = {52, 10, 26, 4, 16, 12, 9, 30, 7};
+		int temp;
+		
+		for (int i = 0; i < array.length-1; i++)
+		{
+			for (int j = 0; j < array.length-i-1; j++)
+			{
+				if (array[j] > array[j+1])
+				{
+					temp = array[j];
+					array[j] = array[j+1];
+					array[j+1] = temp;
+					
+					for (int k = 0; k < array.length; k++)
+					{
+						System.out.print(array[k] + " ");
+					}
+					System.out.println();
+					System.out.println();
+				}
+				
+				
+			}
+			
+		}
+
+	}
+
+}

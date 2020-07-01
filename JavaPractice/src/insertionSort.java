158a2778485057758362b7dc76979f1842681327
Eunnam Ahn <baduk89@hanmail.net>
2020-07-01 18:41:47 -0400

    First commit

---
+++ JavaPractice/src/insertionSort.java
@@ -0,0 +1,38 @@
+public class insertionSort {
+
+	public static void main(String[] args) {
+
+		int [] array = {52, 10, 26, 4, 16, 12, 9, 30, 7};
+		int i, j, k;
+		int temp;
+		int size = array.length;
+		
+		for (i = 1; i < size; i++)
+		{
+			System.out.println("i: " + i);
+			temp = array[i];
+			j = i-1;
+			
+			while ((j >= 0) && (temp < array[j]))
+			{
+				array[j+1] = array[j];
+				j--;
+			}
+			array[j+1] = temp;
+			
+			for (k = 0; k < size; k++)
+			{
+				System.out.print(array[k] + " ");
+			}
+			System.out.println();
+			System.out.println();
+		}
+		
+//		for (k = 0; k < size; k++)
+//		{
+//			System.out.println(array[k]);
+//		}
+		
+	}
+
+}

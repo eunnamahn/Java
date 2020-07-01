5ea1e05432602b0024ea178d27a3f6502992f154
Eunnam Ahn <baduk89@hanmail.net>
2020-07-01 13:12:47 -0400

    First commit

---
+++ JavaPractice/src/selectionSort.java
@@ -0,0 +1,40 @@
+
+public class selectionSort {
+
+	public static void main(String[] args) {
+		
+		int [] array = {52, 10, 26, 4, 16, 12, 9, 30, 7};
+		int i, j, k;
+		int minValue, minIndex, temp;
+		int size = array.length;
+		
+		for (i = 0; i < size-1; i++)
+		{
+			minIndex = i;
+			
+			for (j = i+1; j < size; j++)
+			{
+				if (array[j] < array[minIndex])
+				{
+					minIndex = j;
+				}
+				
+			}
+			
+			temp = array[minIndex];
+			array[minIndex] = array[i];
+			array[i] = temp;
+				
+			System.out.println("i: " + i);
+			for (k = 0; k < size; k++)
+			{
+				System.out.print(array[k] + " ");
+			}
+			System.out.println();
+			System.out.println();
+			
+		}
+
+	}
+
+}

4c3fcf03951ccfef5c21d581679aa550f826cea0
Eunnam Ahn <baduk89@hanmail.net>
2020-07-04 09:45:42 -0400

    Minor edit

--- JavaPractice/src/maxSum.java
+++ JavaPractice/src/maxSum.java
@@ -4,8 +4,20 @@ public class maxSum {
 
 	public static void main(String[] args) {
 		
-		int data[] = new int[] {-2, 3, 5, -14, 12, 3, -9, 8, -1, 13, 2, -5, 4};
-		int size = data.length;
+//		int data[] = new int[] {-2, 3, 5, -14, 12, 3, -9, 8, -1, 13, 2, -5, 4};
+//		int size = data.length;
+		
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		
+		int data[] = new int[n];
+		
+		for (int i = 0; i < n; i++)
+		{
+			data[i] = scan.nextInt();
+		}
+		
+		int size = n;
 		
 		int max = 0;
 		
